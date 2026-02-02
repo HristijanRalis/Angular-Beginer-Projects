e01f6a18ee166e46c60c6206477b3997dfab8e7b
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2026-02-02 15:06:00 +0100

    IMplemented type and interface, dynamic style change,storing data model in separate files

--- AngularFirstApp/01-starting-project/src/app/user/user.component.ts
+++ AngularFirstApp/01-starting-project/src/app/user/user.component.ts
@@ -1,9 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-type User = {
-  id: string;
-  avatar: string;
-  name: string;
-};
+import { type User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -14,6 +10,7 @@ type User = {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected!: boolean;
   @Output() select = new EventEmitter<string>();
   get imagePath() {
     return 'assets/users/' + this.user.avatar;
