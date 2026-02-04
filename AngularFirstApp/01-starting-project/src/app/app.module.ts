4425b9d893b2dc3058dbef8c06e7ab614cc0e914
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-04 11:12:41 +0100

    Implement and another shared module for CardComonent

--- AngularFirstApp/01-starting-project/src/app/app.module.ts
+++ AngularFirstApp/01-starting-project/src/app/app.module.ts
@@ -6,21 +6,20 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header-component';
 import { UserComponent } from './user/user.component';
 import { TasksComponentComponent } from './tasks-component/tasks-component.component';
-import { CardComponent } from './shared/card/card.component';
 import { TaskComponent } from './tasks-component/task/task.component';
 import { NewTaskComponent } from './tasks-component/new-task/new-task.component';
+import { SharedModule } from './shared/card/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     UserComponent,
-    CardComponent,
     TasksComponentComponent,
     TaskComponent,
     NewTaskComponent,
   ],
   bootstrap: [AppComponent],
-  imports: [BrowserModule, FormsModule],
+  imports: [BrowserModule, FormsModule, SharedModule],
 })
 export class AppModule {}
