6719b51ccd8db6902244c5ca6e98860f61b003d2
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-05 11:27:07 +0100

    Done with InvestmentCalculator

--- InvestmentCalculator/src/app/app.component.ts
+++ InvestmentCalculator/src/app/app.component.ts
@@ -1,37 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import type { InvestmentInputType } from './investment-input-model';
+import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, UserInputComponent],
+  imports: [HeaderComponent, UserInputComponent, InvestmentResultsComponent],
   templateUrl: './app.component.html',
 })
-export class AppComponent {
-  onCalculateInvestmentResults(data: InvestmentInputType) {
-    const { initialInvestment, duration, annualInvestment, expectedReturn } =
-      data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-   console.log(annualData)
-  }
-}
+export class AppComponent {}
---
+++ InvestmentCalculator/src/app/investment-results/investment-results.component.ts
@@ -0,0 +1,16 @@
+import { CommonModule } from '@angular/common';
+import { Component, computed, inject } from '@angular/core';
+import { InvestmentService } from '../investment.service';
+
+@Component({
+  selector: 'app-investment-results',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './investment-results.component.html',
+  styleUrl: './investment-results.component.css',
+})
+export class InvestmentResultsComponent {
+  private investmentService = inject(InvestmentService);
+
+  results = computed(() => this.investmentService.resultData());
+}
