3b80a86f501f3e93a5bbe0bfdbc79b138fc33c42
Hristijan Ralevski <hristijanr@gecko.mk>
2026-02-13 10:51:51 +0100

    Defined routes

--- NavigationRouter/navigationRouter/src/app/app.routes.ts
+++ NavigationRouter/navigationRouter/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { AboutUs } from './pages/about-us/about-us';
+import { Contact } from './pages/contact/contact';
+import { Blog } from './pages/blog/blog';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home,
+  },
+  {
+    path: 'aboutUs',
+    component: AboutUs,
+  },
+  {
+    path: 'contactUs',
+    component: Contact,
+  },
+  {
+    path: 'blog',
+    component: Blog,
+  },
+];
