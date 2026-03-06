028cd378ae599a96c40b08fe3f1cc362f8f85d0c
Hristijan Ralevski <hristijanr@gecko.mk>
2026-03-06 15:51:35 +0100

    Now the Standalone Component are clear

--- StandaloneCompnent/src/app/app.module.ts
+++ StandaloneCompnent/src/app/app.module.ts
@@ -1,15 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserModule} from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { SharedModule } from './shared/shared.module';
-import { DetailsComponent } from './welcome/details/details.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, DetailsComponent],
-  imports: [BrowserModule, SharedModule],
+  declarations: [],
+  imports: [BrowserModule, WelcomeComponent, AppComponent],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [],
 })
 export class AppModule {}
