41734c6c4ac708fc7b55231731356650a9d2633b
Hristijan Ralevski <hristijan.ralevski@hotmail.com>
2026-07-08 22:05:22 +0200

    working with signals

--- WorkingWithSignals/src/app/signals/signal.component.ts
+++ WorkingWithSignals/src/app/signals/signal.component.ts
@@ -1,23 +1,24 @@
-import { NgFor } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { NgFor } from "@angular/common";
+import { Component, computed, signal } from "@angular/core";
 
 @Component({
-  selector: 'app-signals',
-  templateUrl: './signal.component.html',
+  selector: "app-signals",
+  templateUrl: "./signal.component.html",
   standalone: true,
   imports: [NgFor],
 })
 export class SignalComponent {
-  actions: string[] = [];
+  actions = signal<string[]>([]);
   counter = signal(0);
+  doubleCounter = computed(() => this.counter() * 2)
 
   increment() {
-    this.counter.update((prev) => prev + 1);
-    this.actions.push('INCREMENT');
+    this.counter.set(this.counter() + 1);
+    this.actions.mutate((oldActions) => oldActions.push("INCREMENT"));
   }
 
   decrement() {
     this.counter.update((prev) => prev - 1);
-    this.actions.push('DECREMENT');
+    this.actions.update((oldActions) => [...oldActions, "DECREMENT"]);
   }
 }
